27c1489817e43608f0c69a627c33f0053aec45ba
tianzhMu <435649705@qq.com>
2015-07-14 23:46:50 +0800

    repair some conflicts

--- dao/account.py
+++ dao/account.py
@@ -2,28 +2,12 @@ from flask import g
 import pymongo
 import db
 
-<<<<<<< HEAD
 def add(userInfo):
   if g.db['account'].find_one({'name' : userInfo.userName}) != NULL:
     return {'result' : 'fail'}
   else:
     g.db.['account'].insert(userInfo)
     return {'result': 'success'}
-=======
-def add(database):
-    posts = database.posts
-    post = {"username":"", 
-            "sid":"",
-            "password":25,
-            "email":"",
-            "grad":"",
-            "school":"",
-            "major":""
-    }
-    post_id = posts.insert(post)
-    print post_id
-  pass
->>>>>>> master
 
 def query(userName):
   user = g.db['account'].find_one({'name' : userName})
@@ -33,16 +17,8 @@ def query(userName):
     return {'result' : 'success', 'msg' : user}
 
 def update(userInfo):
-<<<<<<< HEAD
-  pass
-=======
-<<<<<<< HEAD
   if g.db['account'].find_one({'name' : userInfo.userName}) != NULL:
     return {'result' : 'fail'}
   else:
     g.db.['account'].update({'name' : userInfo.userName}, {'$set' : userInfo})
     return {'result': 'success'}
-=======
-  pass
->>>>>>> master
->>>>>>> tianzh
